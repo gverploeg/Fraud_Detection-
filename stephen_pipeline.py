dbf4cf4edc2525df4e06a3b23706d350528ab192
stephen-lynch <stephen.james.lynch@gmail.com>
2020-12-18 12:43:03 -0700

    finished pipeline

--- stephen_pipeline.py
+++ stephen_pipeline.py
@@ -2,14 +2,17 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 import requests
+import pickle
 
+pickle_off = open('bestRfModel.pkl', 'rb')
+model = pickle.load(pickle_off)
 request = requests.get('http://galvanize-case-study-on-fraud.herokuapp.com/data_point').json()
 cols = ['org_name', 'venue_latitude', 'venue_longitude', 'event_published', 'user_created', 
         'venue_name', 'currency', 'org_desc', 'event_created', 'event_start', 'payee_name', 'payout_type',
         'sale_duration2', 'sale_duration', 'approx_payout_date', 'listed', 'gts', 'num_order', 'venue_state',
         'event_end', 'description', 'object_id', 'country', 'venue_country', 
-        'has_analytics', 'venue_address', 'num_payouts', 'org_twitter', 'has_logo', 'name', 'show_map', 'event_id',
-        'availability', 'quantity_sold', 'user_type']
+        'has_analytics', 'venue_address', 'num_payouts', 'name', 'event_id',
+        'quantity_sold', 'ticket_types']
 
 def loadData(request):
     datapoint = pd.DataFrame.from_dict(request, orient='index').T
@@ -39,12 +42,20 @@ def dropCol(datapoint, cols):
 def createDummies(df):
     df['yahoo'] = np.where(df['email_domain'].str.contains('yahoo'), 1 ,0)
     df['is_org'] = np.where(df['email_domain'].str.contains('.org'), 1 ,0)
+    df['is_fr'] = np.where(df['email_domain'].str.contains('.fr'), 1 ,0)
     df['truth_col'] = df['previous_payouts'].apply(lambda x: x)
     df['prev_payouts0'] = np.where(df['truth_col'] == 0, 1, 0)
     df['has_header_1.0'] = np.where(df['has_header'] == 1.0, 1, 0)
+    df['has_header_0.0'] = np.where(df['has_header'] == 0.0, 1, 0)
+    df['has_header_NaN'] = np.where(df['has_header'].isnull == True, 1, 0)
     df['del_method_3.0'] = np.where(df['delivery_method'] == 3.0, 1, 0)
+    df['del_method_1.0'] = np.where(df['delivery_method'] == 1.0, 1, 0)
+    df['del_method_0.0'] = np.where(df['delivery_method'] == 1.0, 1, 0)
+    df['del_method_NaN'] = np.where(df['delivery_method'].isnull == True, 1, 0)
     df = df.drop(['email_domain', 'truth_col', 'has_header', 'previous_payouts', 'delivery_method'], axis = 1)
     return df
+
+
 
 
 def modelData(request):
@@ -54,8 +65,9 @@ def modelData(request):
     datapoint = createDummies(datapoint)
     return datapoint
 
+def prediction(request):
+    df = modelData(request)
+    return model.predict_proba(df.values)
+
 if __name__ == '__main__':
-    
-    
-    print(modelData(request).columns)
-    
+    print(prediction(request))
