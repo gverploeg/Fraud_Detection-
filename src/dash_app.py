487336141e422a88e3e74a40c593ba086e92fee8
Chris Sanchez <americanthinker@americanthinker-imac.local>
2020-12-21 09:09:33 -0700

    final commit

--- src/dash_app.py
+++ src/dash_app.py
@@ -58,6 +58,7 @@ exec.insert_row(filepath, 'payouts', len(payout_values), payout_values)
 #create risk assessment from risk table data
 risk_table_cols = ['cost', 'availability', 'quantity_total']
 risk_table = ticket_table.loc[:, risk_table_cols]
+data = risk_table.to_dict('records')
 risk_assess = risk(r)
 
 app.layout = html.Div(id='container', style=dict(fontFamily='Garamond'), children=[
@@ -68,16 +69,17 @@ app.layout = html.Div(id='container', style=dict(fontFamily='Garamond'), childre
         html.Br(),
         html.Br(),
         dbc.Col(id='headline', width=12, style=dict(textAlign='center'), children=html.H3(f"Event Name:")),
-        dbc.Col(id='headline2', width=12, style=dict(textAlign='center'), children=html.H3(f"{org_name}")),
+        dbc.Col(id='headline2', width=12, style=dict(textAlign='center'), children=html.H3(f"{org_name}", id='org-name-label')),
         html.Br(),
-        dbc.Col(id='data-table', width=dict(size=6, offset=3), children=dash_table.DataTable(
-            id='table', data=risk_table.to_dict('records'),
-            columns=[{'id': c, 'name':c} for c in risk_table.columns],
-            style_header={'backgroundColor': 'rgb(30, 30, 30)'},
-            style_cell={
-                'backgroundColor': 'rgb(50, 50, 50)',
-                'color': 'white'
-    },
+        dbc.Col(id='data-table', width=dict(size=6, offset=3),
+                children=dash_table.DataTable(
+                        id='table',
+                        data=data,
+                        columns=[{'id': c, 'name':c} for c in risk_table.columns],
+                        style_header={'backgroundColor': 'rgb(30, 30, 30)'},
+                        style_cell={
+                            'backgroundColor': 'rgb(50, 50, 50)',
+                            'color': 'white'},
         )),
         html.Br(),
         html.Br(),
@@ -89,7 +91,7 @@ app.layout = html.Div(id='container', style=dict(fontFamily='Garamond'), childre
                                 daq.Gauge(
                                     id='gauge',
                                     color={"gradient":True,"ranges":{"green":[0,6],"yellow":[6,8],"red":[8,10]}},
-                                    value=0,
+                                    value=predicted_probability * 10,
                                     label=f'Fraud Prediction',
                                     max=10,
                                     min=0)
@@ -106,7 +108,7 @@ app.layout = html.Div(id='container', style=dict(fontFamily='Garamond'), childre
                         ])),
         html.Div(id='risk',
                  children=[
-                        html.H3(risk_assess, style=dict(textAlign='center')),
+                        html.H3(risk_assess, id='risk-label', style=dict(textAlign='center')),
                         dcc.Interval(
                             id='interval-component',
                             interval=1*100, # in milliseconds
@@ -117,14 +119,54 @@ app.layout = html.Div(id='container', style=dict(fontFamily='Garamond'), childre
 
 @app.callback(
     Output('gauge', 'value'),
+    Output('org-name-label', 'children'),
+    Output('table', 'data'),
+    Output('risk-label', 'children'),
     Input('predict-button', 'n_clicks')
 )
 def move_needle(clicks):
     if clicks is None:
         raise PreventUpdate
     elif clicks > 0:
-        prob = round(predicted_probability, 1) * 10
-        return prob
+        r = requests.get('http://galvanize-case-study-on-fraud.herokuapp.com/data_point').json()
+        # create timestamp for use as Primary Key in all database tables
+        stamp = datetime.now().timestamp()
+        # using pickled ML model create prediction from data
+        predicted_probability = prediction(r)[0][1]
+        #prob = round(predicted_probability, 1) * 10
+
+        # munge data for ease of use
+        datapoint = pd.DataFrame.from_dict(r, orient='index').T
+        org_name = datapoint['org_name'][0]
+
+        # drop compound data types for use in other sqlite tables
+        long_data = datapoint.drop(['ticket_types', 'previous_payouts'], axis=1)
+
+        # update with Primary key and fraud predction
+        values = long_data.loc[0].values.tolist()
+        values.insert(0, stamp)
+        values.insert(len(values), predicted_probability)
+        exec.insert_row(filepath, 'longform', 43, values)
+
+        # create ticket_type frame and then insert row in ticket_table
+        ticket_type_cols = ['availability', 'cost', 'event_id', 'quantity_sold', 'quantity_total']
+        ticket_table = ticketTypes(datapoint).loc[:, ticket_type_cols]
+        ticket_values = ticket_table.values.tolist()[0]
+        ticket_values.insert(0, stamp)
+        exec.insert_row(filepath, 'ticket_table', len(ticket_values), ticket_values)
+
+        # create previous_payouts frame and then insert row in payouts table
+        previous_payout = pd.DataFrame.from_dict(datapoint['previous_payouts'][0][0], orient='index').T
+        payout_values = previous_payout.values.tolist()[0]
+        payout_values.insert(0, stamp)
+        exec.insert_row(filepath, 'payouts', len(payout_values), payout_values)
+
+        # create risk assessment from risk table data
+        risk_table_cols = ['cost', 'availability', 'quantity_total']
+        data = ticket_table.loc[:, risk_table_cols].to_dict('records')
+        risk_assess = risk(r)
+        print(predicted_probability)
+        return predicted_probability * 10, org_name, data, risk_assess
 
 if __name__ == '__main__':
     app.run_server(debug=True)
